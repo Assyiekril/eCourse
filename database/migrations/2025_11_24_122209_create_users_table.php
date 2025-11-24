7f4f8c538bae39b7702b76ce0c73c86ce8f07170
Assyiekril <arilsarda21@gmail.com>
2025-11-24 21:46:42 +0800

    Perbaikan Migrations User

--- database/migrations/2025_11_24_122209_create_users_table.php
+++ database/migrations/2025_11_24_122209_create_users_table.php
@@ -10,10 +10,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username'); 
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('role', ['teacher', 'student', 'admin']);
+            $table->boolean('is_active')->default(true); 
             $table->rememberToken();
             $table->timestamps();
         });
